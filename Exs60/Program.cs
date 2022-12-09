26907c5dcd03dcc772fb2045dd2e0a10ddfda1df
Vladimir Stepankin <vladimirstepankin@mail.ru>
2022-12-09 15:44:08 +0300

    Добавил решение задачи № 60

---
+++ Exs60/Program.cs
@@ -0,0 +1,44 @@
+﻿/*Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.*/
+int lists = ReadInt("Введите количество страниц: ");
+int rows = ReadInt("Введите количество строк: ");
+int columns = ReadInt("Введите количество столбцов: ");
+int[,,] numbers = new int[lists, rows, columns];
+FillMatrixRandomNumbers(numbers);
+WriteMatrix(numbers);
+
+void FillMatrixRandomNumbers(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = new Random().Next(10, 100);
+            }
+        }
+    }
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void WriteMatrix(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(2); j++)
+        {
+            for (int k = 0; k < array.GetLength(1); k++)
+            {
+                Console.Write($"{array[j, k, i]}({j},{k},{i}) ");
+            }
+            Console.WriteLine();
+        }
+    }
+    Console.WriteLine();
+}
